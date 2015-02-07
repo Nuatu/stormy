05698f8072f4c3d0629d42701165935a10fd25a2
Nuatu <nuatut@gmail.com>
2015-02-07 00:05:05 -0800

    Add RedisClient and RedisConnection

--- lesson1/stage1/src/jvm/udacity/storm/ReporterExclamationTopology.java
+++ lesson1/stage1/src/jvm/udacity/storm/ReporterExclamationTopology.java
@@ -16,6 +16,10 @@ import backtype.storm.utils.Utils;
 
 import java.util.Map;
 
+import com.lambdaworks.redis.RedisClient;
+import com.lambdaworks.redis.RedisConnection;
+
+
 //********* TO DO 1-of-4 imported http://mvnrepository.com/artifact/com.lambdaworks/lettuce/
 
 
@@ -54,6 +58,7 @@ public class ReporterExclamationTopology {
     //********* TO DO 2-of-4
     // place holder to keep the connection to redis
 
+    RedisConnection<String,String> redis;
 
     //********* END 2-of-4
 
@@ -69,7 +74,11 @@ public class ReporterExclamationTopology {
       //********* TO DO 3-of-4
       // instantiate a redis connection
 
+      RedisClient client = new RedisCLient("localhost",6379);
+
       // initiate the actual connection
+
+      redis = client.connect();
 
       //********* END 3-of-4
     }
@@ -88,8 +97,8 @@ public class ReporterExclamationTopology {
       _collector.emit(tuple, new Values(exclamatedWord.toString()));
 
       //********* TO DO 4-of-4 Uncomment redis reporter
-      //long count = 30;
-      //redis.publish("WordCountTopology", exclamatedWord.toString() + "|" + Long.toString(count));
+      long count = 30;
+      redis.publish("WordCountTopology", exclamatedWord.toString() + "|" + Long.toString(count));
       //********* END 4-of-4
     }
 
